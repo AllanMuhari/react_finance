0972759aaec7344ecb8cdf2674df0eed24808753
AllanMuhari <allanmuhari087@gmail.com>
2023-06-07 01:10:01 +0300

    cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Analytics from './components/Analytics';
 import Newsletter from './components/Newsletter';
+import Cards from './components/Cards';
 
 
 function App() {
