3f6099f535755c3ecb7469605c04262b231e6c43
AllanMuhari <allanmuhari087@gmail.com>
2023-06-07 00:39:13 +0300

    newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className='w-full py-16 text-white'>
+    <div className='w-full py-16 text-white px-4' >
        <div className="max-w-[1240px] mx-auto lg:grid-cols-3">
-           <div>
-               <h1 className='md:text4xl sm:text-3xl text-2xl'>Wants tips & tricks to optimize your flow</h1>
+           <div className='lg:col-span-2 my-4'>
+               <h1 className='md:text4xl sm:text-3xl text-2xl font-bold py-2'>Wants tips & tricks to optimize your flow</h1>
                <p>Sign up to our newsletter and stay up to date</p>
            </div>
+         <div className='my-4'>
+             <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+                <input type="email" placeholder='Enter your email' className='w-full text-black py-2 px-4 rounded-md outline-none' />
+                <button className='bg-[#24fb93] text-black font-medium w-[200px] ml-4 my-6 px-6 py-3 rounded-md'>Subscribe</button>
+             </div>
+         <p>We care abot the protection of your data. Read our <span className='text-[#00df9a]'>Privacy Policy.</span> </p>
+         </div>
        </div>
     </div>
   )
