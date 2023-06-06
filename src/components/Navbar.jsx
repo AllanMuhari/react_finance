7461cefbfa2b8de2f5df43a08398bfe4da52d2d8
AllanMuhari <allanmuhari087@gmail.com>
2023-06-06 09:51:22 +0300

    navbar modificatiion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
@@ -10,9 +11,12 @@ const Navbar = () => {
         <li className='p-4'>Resources</li>
         <li className='p-4'>About</li>
         <li className='p-4'>Contact</li>
-
       </ul>
+      <div>
+        <AiOutlineMenu />
+      </div>
     </div>
+ 
   )
 }
 
