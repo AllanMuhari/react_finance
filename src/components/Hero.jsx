f0ef7e5a53531af9a59f4e72a3f04455bd7d2bde
AllanMuhari <allanmuhari087@gmail.com>
2023-06-06 23:30:13 +0300

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
                 <div className='flex justify-center items-center'>
                     <p className='md:text5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for </p>
-                    <span className='md:text4xl sm:text-3xl text-xl font-bold pt-2 ml-2'>
+                    <span className='md:text4xl sm:text-3xl text-xl font-bold pl-4 ml-2'>
                        <Typewriter
-                         words={['BTB','BTC','SASS', ]}
+                         words={['BTB','BTC','SASS' ]}
                          loop={5}
                          cursor
                          cursorStyle='_'
@@ -21,6 +21,8 @@ const Hero = () => {
                       </span>
         
                 </div>
+                <p className='md:text-2xl text-xl font-bold text-gray-400'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
         </div>
         
         </div>
