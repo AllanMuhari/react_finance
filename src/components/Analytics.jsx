c2420c6baf6eb3ebd1c2561d982e6cba3f2c8edc
AllanMuhari <allanmuhari087@gmail.com>
2023-06-07 00:15:29 +0300

    analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -7,13 +7,14 @@ const Analytics = () => {
        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
               <img src={Laptop} className="w-[500px] mx-auto my-4" />
               <div className='flex flex-col justify-center'>
-                <p>DATA ANALYTICS CENTRALLY</p>
-                <h1>Manage Data Analytics Centrally</h1>
-                <p className='text-[]'>Lorem ipsum dolor,
+                <p className='text-[#00df9a] font-bold'>DATA ANALYTICS CENTRALLY</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+                <p >Lorem ipsum dolor,
                      sit amet consectetur adipisicing elit. 
                      Vero dolorum magni culpa maxime facere atque quibusdam aperiam natus, 
                      quos nesciunt sequi voluptatem sunt maiores quod facilis.
                       Quasi laboriosam est modi?</p>
+                      <button className=' bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
               </div>
        </div>
 
